fdac02789cd8e307154a2cd0a961f7395df5bd3c
neuron-electronics <77750176+neuron-electronics@users.noreply.github.com>
2021-02-28 14:30:28 +0100

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+if ! hash python; then
+    echo "python is not installed"
+    sudo apt-get python
+fi
+python -c "import playsound"
+if echo$? == 1 then
+    pip install playsound
+fi
+python main.py
